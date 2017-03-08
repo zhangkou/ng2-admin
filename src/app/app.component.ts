2de1a69eae385488abf82ba7eb112bbb7f51d864
Zak.zhang <zak.zhang@qq.com>
2017-03-08 17:16:15 +0800

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,3 +49,27 @@ export class App {
     BaThemePreloader.registerLoader(this._imageLoader.load(layoutPaths.images.root + 'sky-bg.jpg'));
   }
 }
+
+
+//workaround
+import { ComponentsHelper } from 'ng2-bootstrap/ng2-bootstrap';
+ComponentsHelper.prototype.getRootViewContainerRef = function () {
+    // https://github.com/angular/angular/issues/9293
+    if (this.root) {
+        return this.root;
+    }
+    var comps = this.applicationRef.components;
+    if (!comps.length) {
+        throw new Error("ApplicationRef instance not found");
+    }
+    try {
+        /* one more ugly hack, read issue above for details */
+        var rootComponent = this.applicationRef._rootComponents[0];
+        //this.root = rootComponent._hostElement.vcRef;
+        this.root = rootComponent._component.viewContainerRef;
+        return this.root;
+    }
+    catch (e) {
+        throw new Error("ApplicationRef instance not found");
+    }
+};
