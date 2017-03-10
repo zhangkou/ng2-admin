ca2f1a2ec279d591bd000e12e35c1b1d8146bad2
Zak.zhang <zak.zhang@qq.com>
2017-03-10 14:14:15 +0800

    .

--- src/app/pages/user/userimport/userimport.component.ts
+++ src/app/pages/user/userimport/userimport.component.ts
@@ -12,6 +12,7 @@ import { BaseComponent } from '../../base/base.component';
 
 export class UserimportComponent {
     users: String ;
+    tableDatas:Array<any>;
 
     constructor(protected restApi: RESTApi, protected route: ActivatedRoute, protected router: Router) {
         
@@ -19,18 +20,41 @@ export class UserimportComponent {
 
     import(target){
       let createUserUrl = "admin/users/" ;
-      if(this.users){
-        let postUsers = [] ;
-        let userArray = this.users.split(";") ;
-        userArray.forEach(userEmail => {
-          let user = {
-            name:   userEmail.split("@")[0],
-            email:  userEmail
-          } ;
-          postUsers.push(user) ;
-        }) ;
-        this.restApi.postData(createUserUrl, {users: postUsers}) ;
-        this.router.navigate(["/pages/user/list"]) ;
+      if(this.tableDatas && this.tableDatas.length > 0){
+        let postPromise = this.restApi.postData(createUserUrl, {users: this.tableDatas}) ;
+        postPromise.then(data => {
+            let importedUsers = data["users"] ;
+            this.tableDatas = importedUsers
+                .map(user => {
+                  return {
+                      name:   user.email.split("@")[0],
+                      email:  user.email,
+                      status: user["message_rest"]["type"],
+                      text:   user["message_rest"]["text"]
+                    } ;
+                }) ;
+        }).catch(error => {
+            console.log(error) ;
+        });
+      }
+    }
+
+    onChange(){
+      if(this.users && this.users.indexOf("@") > 0){
+        let userArray = this.users.replace(/[\r\n]/g, ";").split(";") ;
+        this.tableDatas = userArray
+            .filter(userEmail =>{
+              return !!userEmail && userEmail.indexOf("@") > 0;
+            })
+            .map(userEmail => {
+              return {
+                  name:   userEmail.split("@")[0],
+                  email:  userEmail,
+                  status: "No import"
+              } ;
+            }) ;
+      }else{
+        this.tableDatas = [] ;
       }
     }
 }
